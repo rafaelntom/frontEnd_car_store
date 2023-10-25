2ada88c9014522869bd620f10b40b89e2ac0d8ca
Rafael Tomazini <rafaelnt83@gmail.com>
2023-10-25 09:52:24 -0300

    feat: api url changed to deployed api on render

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://db-car-store-webservice.onrender.com/",
   timeout: 5000,
 });
 
